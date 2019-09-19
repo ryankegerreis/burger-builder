431a674a79d9e21b84d9b733a13362f8eae0f7c4
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-09-18 21:54:06 -0400

    Installed CRA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cat from './images/cat.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Future burger
+      <img src={cat} />
+    </div>
+  );
+}
+
+export default App;
