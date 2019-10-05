08e05b3998b99cb9358a478f67e8daf29938cbc5
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-10-05 12:42:22 -0400

    Build controls built, but CSS is not outputting  properly

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 export default function Burger(props) {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(eachKey => {
       return [...Array(props.ingredients[eachKey])].map((_, i) => {
         return <BurgerIngredient key={eachKey + i} type={eachKey} />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add some ingredients.</p>
+  }
+
+  console.log(transformedIngredients)
   return (
     <div className='Burger'>
       <BurgerIngredient type='bread-top' />
