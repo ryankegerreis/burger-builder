ab7f63b913d0dfb965464b29b24d8d3510dae695
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-10-20 10:38:18 -0400

    Controls for removing ingredients are now available

--- src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -6,7 +6,7 @@ export default function buildControl(props) {
   return (
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
-      <button className={classes.Less}>Less</button>
+      <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
       <button className={classes.More} onClick={props.added}>More</button>
     </div>
   )
